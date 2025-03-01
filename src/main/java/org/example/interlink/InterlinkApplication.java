c619ebea9fa962d560b1f849c340ff97cf7e5c58
jaredthomas01 <jaredthomas570j@gmail.com>
2025-03-01 21:46:20 +0300

    first commit

---
+++ src/main/java/org/example/interlink/InterlinkApplication.java
@@ -0,0 +1,13 @@
+package org.example.interlink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterlinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterlinkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/interlink/InterlinkApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.interlink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterlinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
